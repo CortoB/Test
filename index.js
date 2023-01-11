47be7115d1bc7091d47899aa9aaf36ed83eddcc3
unknown <cortobruzda@gmail.com>
2023-01-11 23:46:09 +0100

    Argv

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import multiply from "./function.js";
+import { argv } from "node:process";
 
-multiply(5, 7);
+const option = argv[2];
+
+if (option === "foo") {
+  console.log("foo");
+} else if (option === "bar") {
+  console.log("bar");
+} else {
+  console.log("default");
+}
